2596ca1551ec19665c61b7e71b31200b8448586c
blessingmad <mad22008@byui.edu>
2023-05-16 19:55:36 +0200

    correcting date

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,4 +1,19 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
 
-const now = new Date();
-document.querySelector("#year").innerHTML = now;
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
+const date = new Date();
+const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+const formattedDate = date.toLocaleDateString("en-US", options);
+
+document.getElementById("datetime").innerHTML = formattedDate;
+
+
+const year = new Date();
+document.querySelector("#year").innerHTML = year;
 
