8472842187e6135eba7722cc57d3d2b6ae4b28a4
blessingmad <mad22008@byui.edu>
2023-06-08 10:16:24 +0200

    book of mormon

--- lesson5/scripts/bom.js
+++ lesson5/scripts/bom.js
@@ -1,7 +1,26 @@
-const input = bate ();
+const input = document.getElementById("favcap");
 
 
-const button = current ();
+const addButton = document.getElementById("submit");
 
 
-const list = list ();
+const list = document.getElementById("list");
+
+//add event listerner to the add chapter button
+addButton.addEventListener("click", function(){
+    const chapterText = input.ariaValueMax.trim();
+
+    if (chapterText !== "") {
+        const li = document.createElement("li");
+    // create delete button
+    const deleteButton = document.createElement("button");deleteButton.textContent = "X"
+    li.textContent = chapterText;
+
+    list.appendChild(li);
+
+    input.value = "";
+
+    input.focus();
+
+    }
+})
