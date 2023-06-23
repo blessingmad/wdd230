b5020e6d10e02fa6c1151716a2fdfafe1462854a
blessingmad <mad22008@byui.edu>
2023-06-23 02:44:00 +0200

    directory added

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,24 +10,31 @@ fetch(url)
 
         contacts.forEach((contact) => {
         let card = document.createElement('section');
-        //let h2 =document.createElement('h2');
-        let name1 = document.querySelector('h2');
-        let iconurl = document.createElement('img');
+        let h2 =document.createElement('h2');
+        let name1 = document.createElement('h2');
+        let address = document.createElement('p')
+        let phone = document.createElement('p')
+        let iconurl = document.createElement('p');
         let homeurl = document.createElement('p');
 
 
 
-        //h2.textContent = `${contact.name1} `;
+        h2.textContent = `${contact.name1} `;
 
         name1.textContent = `Company Name: ${contact.name1}`;
-        iconurl.textContent = `Logo: ${contact.iconurl}`;
-        homeurl = textContent = `Home Page: ${contact.homeurl}`
+        name1.textContent = `Company Address: ${contact.address}`;
+        name1.textContent = `Company Contact: ${contact.phone}`;
+        iconurl.textContent = `Logo URL: ${contact.iconurl}`;
+        homeurl.textContent = `Home Page URL: ${contact.homeurl}`
         card.setAttribute('src', contacts.card);
        // append the section(card) with the created elements
-        //card.appendChild(h2);
+        card.appendChild(h2);
         card.appendChild(name1);
-        card.appendChild(homeurl); 
+        card.appendChild(address);
+        card.appendChild(phone);
         card.appendChild(iconurl);
+        card.appendChild(homeurl); 
+        
         
     
         cards.appendChild(card)
