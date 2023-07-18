f019ef50b434b2de340ec32857ee92ead323521e
blessingmad <mad22008@byui.edu>
2023-07-18 15:47:56 +0200

    push

--- final project/scripts/fresh.js
+++ final project/scripts/fresh.js
@@ -1,5 +1,5 @@
 const fruitUrl = "https://blessingmad.github.io/wdd230/final/scripts/data.json"
-function getElements(selectId, options) {
+function populateSelectElement(selectId, options) {
     const select = document.getElementById(selectId);
 
     for (const option of fruits) {
@@ -9,25 +9,25 @@ function getElements(selectId, options) {
         select.appendChild(optionElement);
     }
 }
-//fetch(fruitUrl)
-//.then(response => response.json())
-//.then(data => { 
-   // const fruits = data.fruits;
-
 fetch(fruitUrl)
-.then (function(response) {
-    return response.json();
+.then(response => response.json())
+.then(data => { 
+    const fruits = data.fruits;
 
-})
-.then(function(jsonData) {
-    const fruits = jsonData["fruits"];
-})
+//fetch(fruitUrl)
+//.then (function(response) {
+   // return response.json();
 
-    getElements('fruits1', fruits);
-    getElements('fruits2', fruits);
-    getElements('fruits3', fruits);
+//})
+//.then(function(jsonData) {
+    //const fruits = jsonData["fruits"];
+//})
 
-//});
+    populateSelectElement('fruits1', fruits);
+    populateSelectElement('fruits2', fruits);
+    populateSelectElement('fruits3', fruits);
+
+});
 document.getElementById('drink-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -35,9 +35,27 @@ document.getElementById('drink-form').addEventListener('submit', function(event)
     const firstName = document.getElementById('first-name').value;
     const email =  document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
-    const fruit1 =  document.getElementById('fruit1').value;
-    const fruit2 = document.getElementById('fruit2').value;
-    const fruit3 =  document.getElementById('fruit3').value;
+    const fruit1 =  document.getElementById('fruit1').name;
+    const fruit2 = document.getElementById('fruit2').name;
+    const fruit3 =  document.getElementById('fruit3').name;
     const instructins = document.getElementById('special-instructions').value;
-  
+    
+    const orderOutput = document.getElementById('order-output');
+   orderOutput.innerHTML = `
+   <h2>Your Order Details:</h2>
+   <p><strong>First Name:</strong> ${firstName}</p>
+   <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Phone Number:</strong> ${phone}</p>
+    <p><strong> Fruit 1:</strong> ${fruit1}</p>
+    <p><strong>Fruit 2:</strong> ${fruit2}</p>
+    <p><strong>Fruit 3:</strong> ${fruit3}</p>
+    <p><strong>Spcisl Instructions:</strong> ${instructions}</p>
+    <p><strong>Order Date:</strong> ${new Date}</p>
+
+
+
+
+
+   `
+
 })
