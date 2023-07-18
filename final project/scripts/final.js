74c791cca047918e37627eb682be39caacb25b57
blessingmad <mad22008@byui.edu>
2023-07-18 13:45:06 +0200

    save all

--- final project/scripts/final.js
+++ final project/scripts/final.js
@@ -1,31 +1,32 @@
-const currentDate = Date();
-document.querySelector("#lastModified").innerHTML = currentDate;
-
-const currentTemp = document.querySelector("current-temp");
-const weatherIcon = document.querySelector("weather-icon");
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
-
+const humidity = document.querySelector("#humidity");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=metric&appid=ec98f94b11c44d888dd6bca93d566cd5";
 
-function displayResults(weatherData){
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong`
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+function displayResults(weatherInfo) {
+    currentTemp.innerHTML = `<strong>${weatherInfo.main.temp.toFixed(0)}</strong`;
+    humidity.innerHTML = `<strong>${weatherInfo.main.humidity.toFixed(0)}</strong`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`;
 
-    const desc = weatherData.weather[0].decription;
+    const desc = weatherData.weather[0].description;
+
     weatherIcon.setAttribute("src", iconsrc);
-    weatherIcon.setAttribute("alt". desc);
+    weatherIcon.setAttribute("alt", desc);
     captionDesc.textContent = desc;
 
 }
-async function apiFetch(){
+
+
+async function apiFetch() {
     try {
         const response = await fetch(url);
-        if (response.ok){
+        if (response.ok) {
             const data = await response.json();
-            console.log(data)
-        }
-        else {
+            console.log(data);
+
+        } else {
             throw Error(await response.text());
         }
     } catch (error) {
@@ -39,3 +40,5 @@ apiFetch();
 
 
 
+const currentDate = Date();
+document.querySelector("#lastModified").innerHTML = currentDate;
